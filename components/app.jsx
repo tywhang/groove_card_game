f52f06b8dc47a6e5817bf35d2b8b6f872691f133
Ted <taeywhang@gmail.com>
2017-04-03 22:45:07 -0700

    Render cards

--- components/app.jsx
+++ components/app.jsx
@@ -4,10 +4,26 @@ import Card from './card.jsx';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+  }
+
+  componentWillMount() {
+    const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const suits = ['Spade', 'Club', 'Diamond', 'Heart'];
+
+    let cards = [];
+    numbers.forEach((number) => {
+      suits.forEach((suit) => {
+        cards.push({ number, suit })
+      });
+    });
+
+    this.setState({ cards });
   }
 
   render() {
-    return <Card />
+    const cards = this.state.cards.map((card, index) => {
+      return <Card key={index} {...card} />;
+    });
+    return <div>{ cards }</div>;
   }
 }
--- components/card.jsx
+++ components/card.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
+const styles = {
+  container: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    margin: '4px',
+    padding: '12px',
+    display: 'inline-block'
+  },
+  innerContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: '60px',
+    width: '60px'
+  }
+}
 
 export default class Card extends Component {
   render() {
-    return <p>Card</p>;
+    return (
+      <div style={styles.container}>
+        <div style={styles.innerContainer}>
+          <span>{ this.props.number }</span>
+          <span>{ this.props.suit }</span>
+        </div>
+      </div>
+    );
   }
 }
