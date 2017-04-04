bece125b0c27e16f5908a269e07b71f3a6198805
Ted <taeywhang@gmail.com>
2017-04-03 22:01:45 -0700

    Initial React Setup

---
+++ components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Card from './card.jsx';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Card />
+  }
+}
---
+++ components/card.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+
+export default class Card extends Component {
+  render() {
+    return <p>Card</p>;
+  }
+}
