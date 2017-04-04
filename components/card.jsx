c51d3ed553ab7bf3dfbe693fdaea4cb2cd2ddd63
Ted <taeywhang@gmail.com>
2017-04-03 23:43:56 -0700

    Cards are flippable

--- components/card.jsx
+++ components/card.jsx
@@ -4,27 +4,39 @@ const styles = {
   container: {
     borderWidth: 1,
     borderStyle: 'solid',
+    cursor: 'pointer',
     margin: '4px',
     padding: '12px',
-    display: 'inline-block'
+    display: 'inline-block',
+    height: '60px',
+    width: '60px'
   },
   innerContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
-    height: '60px',
-    width: '60px'
+    flexDirection: 'column'
   }
 }
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { covered: true };
+  }
+
+  flip() {
+    this.setState({ covered: !this.state.covered });
+  }
+
   render() {
     return (
-      <div style={styles.container}>
+      <div style={styles.container} onClick={this.flip.bind(this)}>
         <div style={styles.innerContainer}>
-          <span>{ this.props.number }</span>
-          <span>{ this.props.suit }</span>
+          { this.state.covered ?
+            <span>?</span> :
+            <span>{ this.props.number } { this.props.suit }</span>
+          }
         </div>
       </div>
     );
