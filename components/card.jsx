adc96929d975c2724b2ca64001cd7ec89f68ea7c
Ted <taeywhang@gmail.com>
2017-04-06 20:05:50 -0700

    Card styles

--- components/card.jsx
+++ components/card.jsx
@@ -11,16 +11,16 @@ const styles = {
     cursor: 'pointer',
     margin: '4px',
     height: '84px',
-    width: '84px'
+    width: '72px'
   },
   inner: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'column',
-    padding: '12px',
-    height: '60px',
-    width: '60px'
+    position: 'relative',
+    height: '84px',
+    width: '72px'
   }
 }
 
@@ -36,6 +36,25 @@ class Card extends Component {
     this.props.flipCard(this.props);
   }
 
+  renderBlackOrRed() {
+    const { suit } = this.props;
+
+    return suit === 'Diamond' || suit === 'Heart' ? { color: 'red' } : { color: 'black' };
+  }
+
+  renderSuit() {
+    switch (this.props.suit) {
+      case 'Spade':
+        return '\u2660';
+      case 'Club':
+        return '\u2663';
+      case 'Diamond':
+        return '\u2666';
+      case 'Heart':
+        return '\u2665';
+    }
+  }
+
   render() {
     return (
       <div style={styles.container} onClick={ this.handleClick.bind(this) }>
@@ -43,8 +62,14 @@ class Card extends Component {
           <div style={styles.inner} /> :
           <div style={styles.inner}>
             { this.props.revealed ?
-              <span>{ this.props.number } { this.props.suit }</span> :
-              <span>?</span>
+              <div style={ this.renderBlackOrRed() }>
+                <span style={{position: 'absolute', top: '2px', left: '2px'}}>{ this.renderSuit() }</span>
+                <span style={{fontSize: '24px'}}>{ this.props.number }</span>
+                <span style={{position: 'absolute', bottom: '2px', right: '2px'}}>{ this.renderSuit() }</span>
+              </div> :
+              <div style={styles.inner}>
+                <span>?</span>
+              </div>
             }
           </div>
         }
